32eeef2306b65bd8026ce8cd15c0d8f3384274ba
MakerCsharp <61178402+MakerCsharp@users.noreply.github.com>
2020-09-22 10:59:15 +0300

    Добавьте файлы проекта.

---
+++ LABA_TOKAR_3_1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LABA_TOKAR_3_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.Integration.WindowsFormsHost host = new System.Windows.Forms.Integration.WindowsFormsHost();
+    MaskedTextBox mtbDate = new MaskedTextBox("00/00/0000");
+    host.Child = mtbDate;
+    this.grid1.Children.Add(host);
+        }
+    }
+}
